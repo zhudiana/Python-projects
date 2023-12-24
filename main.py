02ce0c3e3282098baf6a22836464e1561f476e1a
judiana <yoditfentaw90@gmail.com>
2023-12-24 19:59:19 +0100

    final commit

--- main.py
+++ main.py
@@ -10,11 +10,32 @@ screen.tracer(0)
 
 
 player = Player()
-screen.onkey(player.move, "Up")
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
 screen.listen()
+screen.onkey(player.move, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+    car_manager.create_car()
+    car_manager.move_cars()
+
+
+    #detect car collision
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    #Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.level_up()
+
+
+screen.exitonclick()
--- player.py
+++ player.py
@@ -15,4 +15,13 @@ class Player(Turtle):
     def move(self):
         self.forward(MOVE_DISTANCE)
 
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+
+    def is_at_finish_line(self):
+       if self.ycor() > FINISH_LINE_Y:
+           return True
+       else:
+           return False
 
