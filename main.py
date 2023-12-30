f9d7322a65bda629faf4c354785a79187c3276c2
judiana <yoditfentaw90@gmail.com>
2023-12-30 23:20:10 +0100

    --

---
+++ main.py
@@ -0,0 +1,49 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "eth_without_names.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+data = pandas.read_csv("regions.csv")
+all_states = data["state"].to_list()
+guessed_states = []
+count = 0
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{count} Correct Regions", prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+            new_data = pandas.DataFrame(missing_states)
+            new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        count += 1
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
+        t.write(answer_state)
+
+
+
+screen.exitonclick()
+
+
+
+
+
+# def get_mouse_click_coor(x,y):
+#     print(x,y)
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
