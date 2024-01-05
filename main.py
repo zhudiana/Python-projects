0dff16000074e1bbc660316987f2fd18480e02ab
judiana <yoditfentaw90@gmail.com>
2024-01-05 23:30:59 +0100

    ui-commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+# window.config(padx=20, pady=20, bg="white")
+
+canvas = Canvas(width=200, height=200, bg="white")
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.pack()
+
+window.mainloop()
