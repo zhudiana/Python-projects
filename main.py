d0a1e4f70adadc3e71b06c5ecb5862116ff14e46
judiana <yoditfentaw90@gmail.com>
2024-01-09 20:24:34 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+
+window = Tk()
+window.title("Flashy")
+window.config(width=800,height=526, padx=50, pady=50)
+
+
+
+
+
+
+window.mainloop()
+
