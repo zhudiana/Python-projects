54c0472212c3db83448babe305b17fafaa053cf5
judiana <yoditfentaw90@gmail.com>
2024-01-10 23:17:51 +0100

    --

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
@@ -6,6 +8,12 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+# ------------------ Reading CSV File ------------------- #
+data = pandas.read_csv("data/french_words.csv")
+french_word_list = data["French"].to_list()
+print(random.choice(french_word_list))
+
+# ------------------ UI --------------------------------- #
 canvas = Canvas(width=800,height=526)
 card_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front)
@@ -17,11 +25,11 @@ canvas.grid(column=0, row=0, columnspan=2)
 #Buttons
 right_image = PhotoImage(file="images/right.png")
 right_image_button = Button(image=right_image, highlightthickness=0)
-right_image_button.grid(column=0,row=2)
+right_image_button.grid(column=1,row=2)
 
 wrong_image = PhotoImage(file="images/wrong.png")
 wrong_image_button = Button(image=wrong_image, highlightthickness=0)
-wrong_image_button.grid(column=1,row=2)
+wrong_image_button.grid(column=0,row=2)
 
 
 window.mainloop()
