931c5ddddd734f0bc3713b8ce5230355780a1600
judiana <yoditfentaw90@gmail.com>
2024-01-19 19:31:16 +0100

    --

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-# ------------------ Reading CSV File ------------------- #
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
 current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 def next_card():
@@ -20,40 +25,47 @@ def next_card():
     flip_timer = window.after(3000, func=flip_card)
 
 
-
 def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
 
 
-# ------------------ UI --------------------------------- #
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-flip_timer =  window.after(3000, func=flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
-french_word = random.choice(to_learn)
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
 card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-card_word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
-canvas.grid(column=0, row=0, columnspan=2)
+canvas.grid(row=0, column=0, columnspan=2)
 
-# Buttons
-right_image = PhotoImage(file="images/right.png")
-right_image_button = Button(image=right_image, highlightthickness=0, command=next_card)
-right_image_button.grid(column=1, row=2)
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
 
-wrong_image = PhotoImage(file="images/wrong.png")
-wrong_image_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
-wrong_image_button.grid(column=0, row=2)
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
+
 
 next_card()
 
 window.mainloop()
+
+
+
