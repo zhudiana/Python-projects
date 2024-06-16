b48b9079bc908e43e3bc0762316011bd8d5660e9
judiana <yoditfentaw90@gmail.com>
2024-06-16 11:11:39 +0200

    --

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
+import time
+
+EMAIL = "--"
+PASSWORD = "--"
 
 response=requests.get(
     "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1",
@@ -11,4 +16,20 @@ soup = BeautifulSoup(amazon_html, "lxml")
 price = soup.find(class_="aok-offscreen").getText()
 price_without_currency = price.split("$")[1]
 price_as_float = float(price_without_currency)
-print(price_as_float)
+
+
+product_name = soup.find(id="productTitle").getText().strip()
+
+
+
+if price_as_float < 100:
+    message = f"{product_name} is now {price_as_float}"
+
+    with smtplib.SMTP(YOUR_SMTP_ADDRESS, port=587) as connection:
+        connection.starttls()
+        result = connection.login(YOUR_EMAIL, YOUR_PASSWORD)
+        connection.sendmail(
+            from_addr=YOUR_EMAIL,
+            to_addrs=YOUR_EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}".encode("utf-8")
+        )
